56563aa15c83007c52b599f43225a7f4a7124bc3
Yuriy <yurchest@gmail.com>
2025-03-30 18:20:39 +0300

    xxx

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from database import db_Session
 import core
 import utils
 import json
+from catboost import CatBoostClassifier
+import pandas as pd
+import random
+import joblib
 
+feature_names = ['enc_loans_credit_status_3', 'enc_loans_credit_type_2', 'is_zero_util_0', 'enc_paym_16_0', 'enc_loans_credit_type_5', 'pre_util_16', 'pre_util_17', 'enc_paym_4_1', 'enc_paym_2_1', 'enc_paym_14_0', 'enc_loans_credit_status_5', 'pre_loans_outstanding_5', 'enc_paym_22_0', 'pre_loans_credit_cost_rate_11', 'enc_paym_23_0', 'enc_paym_20_1', 'enc_paym_13_0', 'enc_paym_1_1', 'pre_util_18', 'enc_paym_9_0', 'pre_since_opened_12', 'enc_paym_4_0', 'enc_paym_24_1', 'pre_loans_max_overdue_sum_1', 'enc_paym_18_0', 'enc_paym_5_0', 'enc_paym_1_0', 'enc_paym_19_0', 'enc_paym_3_0', 'is_zero_loans90_0', 'pre_loans_credit_limit_2', 'is_zero_loans6090_0', 'enc_paym_2_0', 'pre_loans_credit_cost_rate_6', 'enc_paym_0_1', 'pre_till_pclose_10', 'is_zero_loans3060_0', 'is_zero_loans530_0', 'enc_loans_credit_type_0', 'pre_util_6', 'pre_util_3']
 
 def get_system_result(input_facts: dict):
     core_object = core.Core(db_Session)
@@ -43,6 +48,42 @@ def add_fact(fact: dict):
 def add_rule(rule: dict):
     core_object = core.Core(db_Session)
     return core_object.add_rule(rule)
+
+def predict_pd(data: pd.DataFrame):
+    model = CatBoostClassifier()
+    model.load_model('model/model.cbm') 
+    # model = joblib.load('model/calibrated_catboost.joblib')
+
+
+    probabilities = model.predict_proba(data)  # возвращает вероятности классов
+    print(f"Предсказание: {probabilities[0][1]}")
+    return probabilities[0][1]
+
+def get_feature_names():
+    model = CatBoostClassifier()
+    # model = joblib.load('model/model.joblib')
+    # return model.estimator.feature_names_
+
+    model.load_model('model/model.cbm') 
+    return model.feature_names_   
+
+
+def get_test_features():
+    def get_random_row_from_pickle(file_path):
+        # Сначала получаем общее количество строк
+        df_len = len(pd.read_pickle(file_path))
+        
+        # Генерируем случайный индекс
+        random_index = random.randint(0, df_len - 1)
+        
+        # Читаем только нужную строку
+        return pd.read_pickle(file_path).iloc[[random_index]]
+
+    df = get_random_row_from_pickle('model/data/data_all.pkl')
+    print(f"target = {df['flag']}")
+    df = df.filter(get_feature_names())
+    return df
+
 
 
 test_input = {
@@ -90,3 +131,6 @@ result = get_system_result(test_input)
 
 
 print(json.dumps(result, indent=2, ensure_ascii=False))
+
+features = get_test_features()
+predict_pd(features)
